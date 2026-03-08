99fcc6959e8271252e820c65eb7bb4b41f9061e0
lakrofn <alvarocrucesmartinez@gmail.com>
2026-03-08 21:48:12 +0100

    Avances.

---
+++ oracle/CONSULTAS/TABLAS_OUTER_JOIN.sql
@@ -0,0 +1,42 @@
+/*
+ALTER SESSION SET "_ORACLE_SCRIPT"=TRUE;
+CREATE USER OUTER_JOIN  IDENTIFIED BY OUTER_JOIN;
+GRANT CONNECT, RESOURCE, DBA TO OUTER_JOIN;
+*/
+
+CREATE TABLE departamento (
+ id NUMBER(10)  PRIMARY KEY,
+ nombre VARCHAR(100) NOT NULL,
+ presupuesto NUMBER(10,2)  NOT NULL,
+ gastos NUMBER(10,2) NOT NULL
+ );
+
+ CREATE TABLE empleado (
+ id NUMBER(10) PRIMARY KEY,
+ nif VARCHAR2(9) NOT NULL UNIQUE,
+ nombre VARCHAR2(100) NOT NULL,
+ apellido1 VARCHAR2(100) NOT NULL,
+ apellido2 VARCHAR2(100),
+ id_departamento NUMBER (10),
+ FOREIGN KEY (id_departamento) REFERENCES departamento(id)
+ );
+ 
+INSERT INTO departamento VALUES(2, 'Sistemas', 150000, 21000);
+INSERT INTO departamento VALUES(3, 'Recursos Humanos', 280000, 25000);
+INSERT INTO departamento VALUES(4, 'Contabilidad', 110000, 3000);
+INSERT INTO departamento VALUES(5, 'I+D', 375000, 380000);
+INSERT INTO departamento VALUES(6, 'Proyectos', 0, 0);
+INSERT INTO departamento VALUES(7, 'Publicidad', 0, 1000);
+INSERT INTO empleado VALUES(1, '32481596F', 'Aarón', 'Rivero', 'Gómez', 2);
+INSERT INTO empleado VALUES(2, 'Y5575632D', 'Adela', 'Salas', 'Díaz', 2);
+INSERT INTO empleado VALUES(3, 'R6970642B', 'Adolfo', 'Rubio', 'Flores', 3);
+INSERT INTO empleado VALUES(4, '77705545E', 'Adrián', 'Suárez', NULL, 4);
+INSERT INTO empleado VALUES(5, '17087203C', 'Marcos', 'Loyola', 'Méndez', 5);
+INSERT INTO empleado VALUES(6, '38382980M', 'María', 'Santana', 'Moreno', 2);
+INSERT INTO empleado VALUES(7, '80576669X', 'Pilar', 'Ruiz', NULL, 2);
+INSERT INTO empleado VALUES(8, '71651431Z', 'Pepe', 'Ruiz', 'Santana', 3);
+INSERT INTO empleado VALUES(9, '56399183D', 'Juan', 'Gómez', 'López', 2);
+INSERT INTO empleado VALUES(10, '46384486H', 'Diego','Flores', 'Salas', 5);
+INSERT INTO empleado VALUES(11, '67389283A', 'Marta','Herrera', 'Gil', 2);
+INSERT INTO empleado VALUES(12, '41234836R', 'Irene','Salas', 'Flores', NULL);
+INSERT INTO empleado VALUES(13, '82635162B', 'Juan Antonio','Sáez', 'Guerrero',NULL);
